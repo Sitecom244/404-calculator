17b804fffe105f5ce589ae2510b3a85ed52041ce
Sitecom244 <jeroen@specishops.nl>
2025-08-20 10:19:37 +0200

    Update fefco-404-calculator.js

--- fefco-404-calculator.js
+++ fefco-404-calculator.js
@@ -286,10 +286,10 @@ document.getElementById('submit-request').addEventListener('click', function(eve
     document.getElementById('submit-request').style.display = 'none';
     document.getElementById('loading-indicator').style.display = 'block';
 
-    // Kleine delay om DOM-updates te verwerken
-    setTimeout(() => {
+    // Wacht tot de DOM echt geüpdatet is vóór het verzenden van het formulier
+    requestAnimationFrame(() => {
         document.querySelector('form').submit();
-    }, 50);
+    });
 });
 
 
@@ -297,3 +297,4 @@ document.getElementById('submit-request').addEventListener('click', function(eve
     calcformCalculate();
     updateWarningAndButton();
 });
+
